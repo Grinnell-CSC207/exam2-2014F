e341d63227c9ed7e48cc9a25152d62c1d6f834ac
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-11-25 23:11:35 -0600

    Add first version of problem 1.

---
+++ Problem1/Experiment.java
@@ -0,0 +1,59 @@
+import java.io.PrintWriter;
+
+import java.math.BigInteger;
+
+import java.util.Random;
+
+/**
+ * A quick experiment with primality testing.
+ */
+public class Experiment
+{
+  // +----------+--------------------------------------------------------
+  // | Settings |
+  // +----------+
+
+  /**
+   * The number of prime experiments to run.
+   */
+  static final int PRIME_EXPERIMENTS = 5;
+
+  /**
+   * The number of composite experiments to run.
+   */
+  static final int COMPOSITE_EXPERIMENTS = 5;
+
+  /**
+   * The bit length of the values we're testing.
+   */
+  static final int BITLEN = 100;
+
+  // +------+------------------------------------------------------------
+  // | Main |
+  // +------+
+
+  public static void main(String[] args)
+  {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    Random random = new Random();
+
+    // A few expected primes.
+    for (int i = 0; i < PRIME_EXPERIMENTS; i++)
+      {
+        BigInteger candidate = BigInteger.probablePrime(BITLEN, random);
+        pen.println("probablyPrime(p=" + candidate + "): " + 
+                    Utils.probablyPrime(candidate));
+      } // for i
+
+    // A few expected composites.
+    for (int i = 0; i < COMPOSITE_EXPERIMENTS; i++)
+      {
+        BigInteger candidate1 = BigInteger.probablePrime(BITLEN/2, random);
+        BigInteger candidate2 = BigInteger.probablePrime(BITLEN/2+1, random);
+        BigInteger candidate = candidate1.multiply(candidate2);
+        pen.println("probablyPrime(c=" + candidate + "): " + 
+                    Utils.probablyPrime(candidate));
+      } // for i
+  } // main(String[])
+ 
+} // class Experiment
---
+++ Problem1/Utils.java
@@ -0,0 +1,55 @@
+import java.math.BigInteger;
+
+import java.util.Random;
+
+/**
+ * Utils.java
+ *   Assorted utilities.
+ */
+public class Utils
+{
+  // +-----------+-------------------------------------------------------
+  // | Constants |
+  // +-----------+
+
+  /**
+   * The number of repetitions we do for a standard primality test.
+   */
+  static final int STANDARD_PRIME_TESTS = 100;
+
+  // +---------+---------------------------------------------------------
+  // | Globals |
+  // +---------+
+
+  /**
+   * A random-number generator, since we use them a lot.
+   */
+  static final Random random = new Random();
+
+  // +----------------+--------------------------------------------------
+  // | Static Methods |
+  // +----------------+
+
+  /**
+   * Compute x^n mod m.
+   */
+  public static BigInteger expmod(BigInteger x, BigInteger n, BigInteger m)
+  {
+    // STUB
+    return BigInteger.ONE;
+  } // expmod(BigInteger)
+
+  /**
+   * Determine whether p is likely to be prime.
+   */
+  public static boolean probablyPrime(BigInteger p)
+  {
+    // Generate i
+    BigInteger i = (new BigInteger(p.bitLength(), random)).remainder(p);
+    // Compute i^(p-1) mod p.  
+    // STUB
+    BigInteger tmp = expmod(i,BigInteger.TEN,p);
+    // Determine whether or not we have 1.
+    return tmp.equals(BigInteger.ONE);
+  } // probablyPrime
+} // Utils
