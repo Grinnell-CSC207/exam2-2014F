04b412bafd0face33e26032c98b56c1afe2017e6
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-11-29 12:55:51 -0600

    New experiments for problem 2.

--- Problem2/src/LinearStructureExpt.java
+++ Problem2/src/LinearStructureExpt.java
@@ -1,23 +1,27 @@
+import java.io.PrintWriter;
+import java.util.Iterator;
+
 /**
  * Experiments with LinearStructures.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
  */
 public class LinearStructureExpt
 {
-  // +----------------+--------------------------------------------------
-  // | Static Methods |
-  // +----------------+
+  // +-------------+-----------------------------------------------------
+  // | Experiments |
+  // +-------------+
 
   /**
    * A series of experiments on a structure of strings.
    */
-  public static void expt01(LinearStructure<String> ls, String prefix)
+  public static void expt01(PrintWriter pen, LinearStructure<String> ls, 
+      String prefix)
     throws Exception
   {
     ReportingLinearStructure<String> expt =
-        new ReportingLinearStructure<String>(ls, prefix);
+        new ReportingLinearStructure<String>(ls, pen, prefix);
+    pen.println("Running experiment 01");
     expt.info();
     expt.put("a");
     expt.info();
@@ -42,5 +46,72 @@ public class LinearStructureExpt
     expt.put("j");
     expt.info();
     expt.clear();
+    pen.println();
   } // expt01
+
+  /**
+   * Another series of experiments, this time with some fun with the
+   * iterator.
+   */
+  public static void expt02(PrintWriter pen, LinearStructure<String> ls, 
+      String prefix)
+    throws Exception
+  {
+    ReportingLinearStructure<String> expt =
+        new ReportingLinearStructure<String>(ls, pen, prefix);
+    String[] strings = new String[] { "e", "f", "g", "h", "i", "j" };
+
+    pen.println("Running experiment 02");
+
+    // Add a bunch of strings (extra a's and b's for remove tests)
+    for (String str : strings)
+      {
+        expt.put("a");
+        expt.put("b");
+        expt.put(str);
+      } // for
+    // Add a few more a's at the end (not that they'll stay there ...)
+    expt.put("a");
+    expt.put("a");
+    expt.put("a");
+    expt.put("a");
+    // Print the whole thing
+    expt.info();
+    // Remove all the e's
+    pen.println("Removing \"e\"");
+    remove(pen, expt, "e");
+    expt.info();
+    // Remove all the a's
+    pen.println("Removing \"a\"");
+    remove(pen, expt, "a");
+    expt.info();
+    // Remove all the b's
+    pen.println("Removing \"b\"");
+    remove(pen, expt, "b");
+    expt.info();
+    // Remove all the f's
+    pen.println("Removing \"f\"");
+    remove(pen, expt, "f");
+    expt.info();
+
+    pen.println();
+  } // expt02(LinearStructure<String>, String)
+
+  // +-----------+-------------------------------------------------------
+  // | Utilities |
+  // +-----------+
+
+  /**
+   * Remove all the copies of str from the structure.
+   */
+  static void remove(PrintWriter pen, ReportingLinearStructure<String> expt,
+      String str)
+  {
+    Iterator<String> it = expt.iterator();
+    while (it.hasNext())
+      {
+        if (str.equals(it.next()))
+          it.remove();
+      } // while
+  } // remove(PrintWriter, ReportingLinearStructure<String>, String)
 } // class LSExpt
--- Problem2/src/RandomLinearStructureExpt.java
+++ Problem2/src/RandomLinearStructureExpt.java
@@ -1,3 +1,5 @@
+import java.io.PrintWriter;
+
 /**
  * Simple experiments with various implementations of random linear
  * structures.
@@ -13,7 +15,9 @@ public class RandomLinearStructureExpt
   public static void main(String[] args)
     throws Exception
   {
+    PrintWriter pen = new PrintWriter(System.out, true);
     RandomLinearStructure<String> rls = new RandomLinearStructure<String>(4);
-    LinearStructureExpt.expt01(rls, "rls.");
+    LinearStructureExpt.expt01(pen, rls, "rls.");
+    LinearStructureExpt.expt02(pen, rls, "rls.");
   } // main(String[])
 } // class RandomLinearStructureExpt
